6c53e9d295adad85e2bd10fac2ea8f64d6d5525b
student <student@B206-10>
2023-02-27 10:14:31 +0700

    Добавьте файлы проекта.

---
+++ PZ_linq_DeamKMS/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_linq_DeamKMS
+{
+    class Book
+    {
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+        public Book(string Author, int Year, int Pages)
+        {
+            this.Author = Author;
+            this.Year = Year;
+            this.Pages = Pages;
+        }
+    }
+}
---
+++ PZ_linq_DeamKMS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_linq_DeamKMS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> list = new List<Book>()
+            {
+                new Book("Шишкин",2002,213),
+                new Book("Плюшкин",3001,213),
+                new Book("Соснов",1991,213),
+                new Book("Куршанашвили",1900,213),
+                new Book("Пупкин",1969,213),
+                new Book("Свисток",2013,213)
+
+            };
+            var listbook = list.Where(u=>u.Year<=1994 && u.Pages>200).ToList();
+            listbook.ForEach(book =>Console.WriteLine(book.Author+" " + book.Year +" " + book.Pages));
+            Console.ReadKey();
+
+        }
+            
+        }
+    }
+
